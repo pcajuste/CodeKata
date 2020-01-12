ff0db6def06812e9ee832a80bcf27cfb8f0e880c
lesly Dorval <ladorval@gmail.com>
2020-01-11 23:13:14 -0500

    Define db manipulation commands to use at the cli to manipulate the new CTT database

--- ct_transit/services/web/manage.py
+++ ct_transit/services/web/manage.py
@@ -1,7 +1,13 @@
 from flask.cli import FlaskGroup
-from video_pull import app
+from video_pull import app, db
 
 cli = FlaskGroup(app)
+
+@cli.command("create_db")
+    def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
